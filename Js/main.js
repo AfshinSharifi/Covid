60a1716b10aa591ed54de95b6ebb97960eeec262
AfshinSharifi <afshinsharifi1376@gmail.com>
2022-09-13 21:38:55 +0430

    changes

--- Js/main.js
+++ Js/main.js
@@ -2,16 +2,18 @@ const casesNumber = document.querySelectorAll("footer p");
 const search = document.querySelector("#case");
 const label = document.querySelector("label");
 const ul = document.querySelector(".result ul");
+const compare = document.querySelector(".compare");
 
 //Global variables
-isSearch = true;
+let isSearch = true;
+let isCompare = true;
 
 //Add Event Listeners
 document.addEventListener("DOMContentLoaded", getCases);
 
 //Functions
 function getCases() {
-  let cases = axios.get("./Cases.json");
+  const cases = axios.get("https://covid-api.mmediagroup.fr/v1/cases");
   cases.then((response) => {
     const allCases = response.data;
     updateCases(allCases.Global.All.confirmed, 8000, casesNumber[0]);
@@ -29,19 +31,8 @@ function getCases() {
     search.addEventListener("keyup", () => {
       searchCases(allCases);
     });
-    label.addEventListener("click", () => {
-      if (isSearch) {
-        search.style.width = "50%";
-        search.style.opacity = "1";
-        isSearch = false;
-      } else {
-        if (!search.value) {
-          search.style.width = "0";
-          search.style.opacity = "0";
-        }
-        isSearch = true;
-      }
-    });
+    label.addEventListener("click", searchInput);
+    compare.addEventListener("click", compareCase);
   });
 }
 
@@ -91,12 +82,19 @@ function showCases(value) {
   search.style.width = "0";
   search.style.opacity = "0";
   isSearch = true;
-  const title = document.querySelector(".description");
-  title.innerHTML = `<img src="https://countryflagsapi.com/png/${value.abbreviation.toUpperCase()}"/><h2>${
-    value.country
-  }</h2>
+  compare.style.display = "block";
+  if (isCompare) {
+    const title = document.querySelector(".description");
+
+    title.innerHTML = `<img src="https://countryflagsapi.com/png/${value.abbreviation.toUpperCase()}"/><h2>${
+      value.country
+    }</h2>
   <p class="capital">${value.capital_city}</p>
   <div class="data flex column">
+        <span style="${changeColor(
+          value.population,
+          value.confirmed
+        )}">status</span>
               <table>
                 <tr>
                   <th><h3>Population</h3></th>
@@ -112,9 +110,9 @@ function showCases(value) {
                 </tr>
                 <tr>
                   <th><h3>Recovered</h3></th>
-                  <th>${numeral(value.confirmed - value.confirmed / 2).format(
-                    "0,0"
-                  )}</th>
+                  <th>${numeral(
+                    value.confirmed - (value.confirmed * 3) / 4
+                  ).format("0,0")}</th>
                 </tr>
                 <tr>
                   <th><h3>Life Expectancy</h3></th>
@@ -126,4 +124,74 @@ function showCases(value) {
                 </tr>
                 </table>
             </div>`;
+  } else {
+    const title = document.querySelector(".covid-img");
+    title.innerHTML = `<img src="https://countryflagsapi.com/png/${value.abbreviation.toUpperCase()}"/><h2 class="headDes">${
+      value.country
+    }</h2>
+  <p class="capital">${value.capital_city}</p>
+  <div class="data flex column">
+        <span style="${changeColor(
+          value.population,
+          value.confirmed
+        )}">status</span>
+              <table>
+                <tr>
+                  <th><h3>Population</h3></th>
+                  <th>${numeral(value.population).format("0,0")}</th>
+                </tr>
+                <tr>
+                  <th><h3>Confirmed</h3></th>
+                  <th>${numeral(value.confirmed).format("0,0")}</th>
+                </tr>
+                <tr>
+                  <th><h3>Deaths</h3></th>
+                  <th>${numeral(value.deaths).format("0,0")}</th>
+                </tr>
+                <tr>
+                  <th><h3>Recovered</h3></th>
+                  <th>${numeral(
+                    value.confirmed - (value.confirmed * 3) / 4
+                  ).format("0,0")}</th>
+                </tr>
+                <tr>
+                  <th><h3>Life Expectancy</h3></th>
+                  <th>${value.life_expectancy} % </th>
+                </tr>
+                <tr>
+                  <th><h3>Vaccinations</h3></th>
+                  <th>${numeral(value.population * (3 / 4)).format("0,0")}</th>
+                </tr>
+                </table>
+            </div>`;
+    isCompare = true;
+    compare.style.display = "none";
+    search.setAttribute("disabled", "true");
+  }
+}
+function changeColor(population, confirmed) {
+  const color = confirmed / population.toFixed(2) + 0.2;
+  if (color > 0.3) {
+    return `background : rgba(235, 77, 75,${color})`;
+  } else {
+    return `background : rgba(249, 202, 36,${color})`;
+  }
+}
+function searchInput() {
+  if (isSearch) {
+    search.style.width = "50%";
+    search.style.opacity = "1";
+    isSearch = false;
+  } else {
+    if (!search.value) {
+      search.style.width = "0";
+      search.style.opacity = "0";
+    }
+    isSearch = true;
+  }
+}
+function compareCase() {
+  searchInput();
+  search.focus();
+  isCompare = false;
 }
